c45b96a302d717b0fe5e709527ff4ab2848e4672
0ettam02 <matteoaruta6@gmail.com>
2024-12-14 13:21:47 +0100

    resolved hydration problem and added some modify

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,14 @@
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
+const inter = Inter({ subsets: ["latin"] });
+import { Inter } from "next/font/google";
 export const metadata = {
-  title: "Home Page",
-  description: "LibreriaACD Home Page",
+  title: "Login",
+  description: "HiveMind Login Page",
 };
-
-export default function RootLayout({ children }) {
+export default function LoginLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,17 @@
-import Footer from "./component/footerComponent"
-import Header from "./component/headerComponent"
+import Footer from "./component/footerComponent";
+import Header from "./component/headerComponent";
+import Login from "./component/logInComponent";
+
+export default function LoginPage() {
 
 
-export default function Home() {
-  return (
-    <>
-    {/* HOMEPAGE */}
-    <Header />
-    {/* FOOTER */}
-    <Footer />
-    </>
+
+    return (
+        <>
+
+            <Login />
+
+        </>
   );
+
 }
